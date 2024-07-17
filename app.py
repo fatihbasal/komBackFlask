efbb1a83814cdbb0d342f919490dbd3792717702
Ömer Bıçakcıoğlu <omer.bicakcioglu@ug.bilkent.edu.tr>
2024-07-17 11:22:38 +0300

    c
    c

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from resources.user import blp as UserBlueprint
 
 from db import db
 import models
-import psycopg2
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -16,16 +16,21 @@ app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345@localhost/kombinledb"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:06mrtnsrn@db/kombinledb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
 with app.app_context():
     db.create_all()
 
 
 api = Api(app)
+
+@app.route("/")
+def index():
+    return "Hello World!"
 
 api.register_blueprint(UserBlueprint)
 
--- models/userdb.py
+++ models/userdb.py
@@ -4,5 +4,14 @@ class UserModel(db.Model):
     __tablename__ = "users"
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            # Add other fields as necessary
+        }
